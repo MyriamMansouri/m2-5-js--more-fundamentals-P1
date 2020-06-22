70b329ddd06931c84c01c10d1af748c24e1fd146
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-22 13:10:59 -0400

    Exercises P1 (1-1 to 1-4)

--- workshop/exercises-P1_theory/exercise-1.js
+++ workshop/exercises-P1_theory/exercise-1.js
@@ -20,13 +20,27 @@ const reverseString = (str) => {
 
 // Expand each of the following and get the result of the expression
 // 1-1
-square(decrement(square(decrement(3))));
+console.log(square(decrement(square(decrement(3)))));
+// 1. decrement() is called. Returns 2
+// 2. square() is called. Returns 4
+// 3. decrement() is called. Returns 3
+// 2. square() is called. Returns 9
 
 // 1-2
-decrement(decrement(square(square(3))));
+console.log(decrement(decrement(square(square(3)))));
+// 1. square() is called. Returns 9
+// 2. square() is called. Returns 81
+// 3. decrement() is called. Returns 80
+// 2. decrement() is called. Returns 79
 
 // 1-3
-duplicateString(reverseString('hello'));
+console.log(duplicateString(reverseString('hello')));
+// 1. reverseString() is called. Returns 'olleh'
+// 2. duplicateString() is called. Returns 'olleholleh'
 
 // 1-4
-reverseString(duplicateString(duplicateString('foo')));
+console.log(reverseString(duplicateString(duplicateString('foo'))));
+// 1. duplicateString() is called. Returns 'foofoo'
+// 2. duplicateString() is called. Returns 'foofoofoofoo'
+// 2. reverseString() is called. Returns 'oofoofoofoof'
+
